1c75bf11eb8bd0e41a5b9921efdf8dfc593382bf
Kay-Michael Würzner <wuerzner@bbaw.de>
2019-01-07 19:01:55 +0100

    Add matching procedure to ingest

--- tocrify/api/hocr.py
+++ tocrify/api/hocr.py
@@ -3,6 +3,7 @@
 from lxml import etree
 
 import os
+import Levenshtein
 
 ns = {
      'xlink' : "http://www.w3.org/1999/xlink",
@@ -97,10 +98,50 @@ class Hocr:
         represented by the mets.Logical parameter.
         :param Logical logical: The logical element to be ingested. 
         """
-        if logical.label != "Text" or (not self.text):
-            pass
-        print(logical.label)
+        # many structures are (regretfully) only labelled with 'Text'
+        if len(logical.label) > 0 and self.text and logical.label != "Text":
 
-        begin = -1
-        # restriction on match quality, parameter?
-        minimum = len(logical.label) / 2
+            # restriction on match quality, parameter?
+            minimum = len(logical.label) / 2
+            
+            # the moving window
+            begin = -1
+            for k in range(0, len(self.text) - len(logical.label)):
+                distance = Levenshtein.distance(logical.label, self.text[k:k+len(logical.label)])
+                if distance <= minimum:
+                    minimum = distance
+                    begin = k
+                if distance == 0:
+                    break
+
+            # we have a suitable match (i.e. below the quality restriction),
+            # full line labels are assumed,
+            # all lines which contribute to the matching window are collected
+            # to deal with multi-line labels
+            if begin != -1:
+                end = begin + len(logical.label) - 1
+                cmp_lines = []
+                pars = []
+                for l in range(0, end - begin):
+                    # append each line only once!
+                    if (not cmp_lines) or cmp_lines[-1] != self.index_struct[begin + l]:
+                        cmp_lines.append(self.index_struct[begin + l])
+                        # get paragraph of line
+                        par = cmp_lines[-1].getparent()
+                        # add it to the first paragraph containing the match and subsequenty
+                        # move the lines around in order to create a single paragraph
+                        # for each match
+                        if not pars:
+                            pars.append(par)
+                        elif pars[0] != par:
+                            pars[0].append(self.index_struct[begin + l])
+                            par.remove(self.index_struct[begin + l])
+                            # eventually delete paragraph
+                            if len(par) == 0:
+                                par.getparent().remove(par)
+
+                # TODO: compare number of lines in paragraph to number of matched lines!!!
+                print(len(pars[0]),len(cmp_lines))
+
+                for cmp_line in cmp_lines:
+                    print(cmp_line.text)
